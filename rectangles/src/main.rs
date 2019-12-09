b03c6bd6a8e9f6b79840e703d050be7719313aaa
mwilly <mwilly-contractor@cars.com>
2019-12-09 11:18:07 -0700

    add a function for creating a square

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -28,4 +32,8 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+
+    println!("sq is {:#?}", sq);
 }
